ccf068fa88066ba7dad11ee27ec885e9c291daed
smb261-panda <grizzi@ethz.ch>
2023-11-29 12:50:17 +0100

    Add speedup by destroying and initializing subscribers

--- scripts/keypoints_perception.py
+++ scripts/keypoints_perception.py
@@ -37,12 +37,20 @@ class PerceptionModule:
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
         # ROS
-        self.calibration_sub = rospy.Subscriber(self.calibration_topic, CameraInfo, self._calibration_callback, queue_size=1)
-        self.image_sub = rospy.Subscriber(self.image_topic, Image, self._image_callback, queue_size=1)
-        self.depth_sub = rospy.Subscriber(self.depth_topic, Image, self._depth_callback, queue_size=1)
+        self._init_subscribers()
         self.marker_pub = rospy.Publisher("perception", MarkerArray, queue_size=1)
         self.detection_srv_client = rospy.ServiceProxy("detect", KeypointsDetection)
         self.trigger_detection_srv = rospy.Service("perceive", KeypointsPerception, self.perceive)
+
+    def _init_subscribers(self):
+        self.calibration_sub = rospy.Subscriber(self.calibration_topic, CameraInfo, self._calibration_callback, queue_size=1)
+        self.image_sub = rospy.Subscriber(self.image_topic, Image, self._image_callback, queue_size=1)
+        self.depth_sub = rospy.Subscriber(self.depth_topic, Image, self._depth_callback, queue_size=1)
+
+    def _destroy_subscribers(self):
+        self.calibration_sub.unregister()
+        self.image_sub.unregister()
+        self.depth_sub.unregister()
 
     def _make_marker(self, id, frame_id, x, y, z):
         marker = Marker()
@@ -97,72 +105,76 @@ class PerceptionModule:
             rospy.logwarn("No rgb image received yet, skipping detection")
             return
 
-        req = KeypointsDetectionRequest()
-        req_depth = copy.deepcopy(self.depth) # store locally the current depth image as well
-        req.rgb = self.rgb
-        res_det: KeypointsDetectionResponse = self.detection_srv_client.call(req)
+        self._destroy_subscribers()
+        try:
+            req = KeypointsDetectionRequest()
+            req_depth = copy.deepcopy(self.depth) # store locally the current depth image as well
+            req.rgb = self.rgb
+            res_det: KeypointsDetectionResponse = self.detection_srv_client.call(req)
 
-        # The check for the correct number of keypoints is already implemented in the keypoint detection service
+            # The check for the correct number of keypoints is already implemented in the keypoint detection service
 
-        marker_array = MarkerArray() # For visualization
-        pose_array = PoseArray()     # For usage
-        pose_array.header.frame_id = self.base_frame
+            marker_array = MarkerArray() # For visualization
+            pose_array = PoseArray()     # For usage
+            pose_array.header.frame_id = self.base_frame
 
-        # let it fail if not found
-        trans = self.tf_buffer.lookup_transform(self.base_frame, self.camera_frame, self.depth_header.stamp, timeout=rospy.Duration(3.0))
-        t_w_cam = np.array([trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z])
-        q = np.array([trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w])
-        R_w_cam = Rotation.from_quat(q).as_matrix()
+            # let it fail if not found
+            trans = self.tf_buffer.lookup_transform(self.base_frame, self.camera_frame, self.depth_header.stamp, timeout=rospy.Duration(3.0))
+            t_w_cam = np.array([trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z])
+            q = np.array([trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w])
+            R_w_cam = Rotation.from_quat(q).as_matrix()
 
-        for i, kpt in enumerate(res_det.detection.keypoints):
-            print("Processing keypoint {} {}".format(kpt.x, kpt.y))
+            for i, kpt in enumerate(res_det.detection.keypoints):
+                print("Processing keypoint {} {}".format(kpt.x, kpt.y))
 
-            # take a window of depths and extract the median to remove outliers
-            # note that the window scales by depth_window_size in all directions
-            x_min, x_max = max(int(kpt.x) - self.depth_window_size, 0), min(int(kpt.x) + self.depth_window_size, req_depth.shape[1]-1)
-            y_min, y_max = max(int(kpt.y) - self.depth_window_size, 0), min(int(kpt.y) + self.depth_window_size, req_depth.shape[0]-1)
+                # take a window of depths and extract the median to remove outliers
+                # note that the window scales by depth_window_size in all directions
+                x_min, x_max = max(int(kpt.x) - self.depth_window_size, 0), min(int(kpt.x) + self.depth_window_size, req_depth.shape[1]-1)
+                y_min, y_max = max(int(kpt.y) - self.depth_window_size, 0), min(int(kpt.y) + self.depth_window_size, req_depth.shape[0]-1)
 
-            # # take depth window
-            depth_wdw = req_depth[y_min: y_max, x_min: x_max]
-            depth_wdw = np.reshape(depth_wdw, (-1,))
-            depth_wdw = depth_wdw[depth_wdw>0]  # threshold invalid values
-            depth_wdw = depth_wdw[depth_wdw<self.max_distance_threshold]  # threshold far away values
+                # # take depth window
+                depth_wdw = req_depth[y_min: y_max, x_min: x_max]
+                depth_wdw = np.reshape(depth_wdw, (-1,))
+                depth_wdw = depth_wdw[depth_wdw>0]  # threshold invalid values
+                depth_wdw = depth_wdw[depth_wdw<self.max_distance_threshold]  # threshold far away values
 
-            if len(depth_wdw) == 0:
-                raise Exception(f"No valid depth information could be extracted for keypoint ({kpt.x}, {kpt.y}), maybe it is too close to the edge of the image")
+                if len(depth_wdw) == 0:
+                    raise Exception(f"No valid depth information could be extracted for keypoint ({kpt.x}, {kpt.y}), maybe it is too close to the edge of the image")
 
-            P_cam = np.zeros((3,))
-            if self.depth_window_mode == "median":
-                P_cam[2] = np.median(depth_wdw)
-            elif self.depth_window_mode == "min":
-                P_cam[2] = np.min(depth_wdw)
-            elif self.depth_window_mode == "max":
-                P_cam[2] = np.max(depth_wdw)
-            else:
-                raise Exception(f"Depth window mode {self.depth_window_mode} is not supported")
-            rospy.loginfo(f"Using depth window mode {self.depth_window_mode} with window size {self.depth_window_size}")
+                P_cam = np.zeros((3,))
+                if self.depth_window_mode == "median":
+                    P_cam[2] = np.median(depth_wdw)
+                elif self.depth_window_mode == "min":
+                    P_cam[2] = np.min(depth_wdw)
+                elif self.depth_window_mode == "max":
+                    P_cam[2] = np.max(depth_wdw)
+                else:
+                    raise Exception(f"Depth window mode {self.depth_window_mode} is not supported")
+                rospy.loginfo(f"Using depth window mode {self.depth_window_mode} with window size {self.depth_window_size}")
 
-            # retrieve the global x, y coordinate of the point given z
-            # TODO not accounting for distortion --> would be better to just operate on undistorted images
-            P_cam[0] = (kpt.x - self.camera_info.K[2]) * P_cam[2] / self.camera_info.K[0]
-            P_cam[1] = (kpt.y - self.camera_info.K[5]) * P_cam[2] / self.camera_info.K[4]
+                # retrieve the global x, y coordinate of the point given z
+                # TODO not accounting for distortion --> would be better to just operate on undistorted images
+                P_cam[0] = (kpt.x - self.camera_info.K[2]) * P_cam[2] / self.camera_info.K[0]
+                P_cam[1] = (kpt.y - self.camera_info.K[5]) * P_cam[2] / self.camera_info.K[4]
 
-            P_base = R_w_cam @ P_cam + t_w_cam
-            marker = self._make_marker(i, self.base_frame, P_base[0], P_base[1], P_base[2])
-            marker_array.markers.append(marker)
-            pose_array.poses.append(marker.pose)
-            print(f"Keypoint {i} perceived at P_cam={P_cam} and P_base={P_base}")
+                P_base = R_w_cam @ P_cam + t_w_cam
+                marker = self._make_marker(i, self.base_frame, P_base[0], P_base[1], P_base[2])
+                marker_array.markers.append(marker)
+                pose_array.poses.append(marker.pose)
+                print(f"Keypoint {i} perceived at P_cam={P_cam} and P_base={P_base}")
 
-        res = KeypointsPerceptionResponse()
-        res.camera_pose.position = trans.transform.translation
-        res.camera_pose.orientation = trans.transform.rotation 
-        res.keypoints2d = res_det.detection.keypoints
-        res.header.stamp = rospy.get_rostime()
-        res.header.frame_id = self.camera_frame
-        res.camera_info = self.camera_info
-        res.keypoints = pose_array
-        self.marker_pub.publish(marker_array)
-        return res
+            res = KeypointsPerceptionResponse()
+            res.camera_pose.position = trans.transform.translation
+            res.camera_pose.orientation = trans.transform.rotation 
+            res.keypoints2d = res_det.detection.keypoints
+            res.header.stamp = rospy.get_rostime()
+            res.header.frame_id = self.camera_frame
+            res.camera_info = self.camera_info
+            res.keypoints = pose_array
+            self.marker_pub.publish(marker_array)
+            return res
+        finally:
+            self._init_subscribers()
 
 
 if __name__ == "__main__":
